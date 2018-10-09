0af10db91cbda1fdf654d4fc06efdf2dc8e98564
Philipp Lüthi <philippluca.luethi@gmail.com>
2018-10-09 19:27:12 +0200

    Added changed to MNIST Dataset

--- CreateDistributedLabels.py
+++ CreateDistributedLabels.py
@@ -16,27 +16,33 @@ def generate_new_labels(challengeIDs, labels, p , count):
     return np.array([upsampled, new_labels]).transpose()
 
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_digits
 X, y = load_iris(True)
+X, y = load_digits(return_X_y=True)
 
 # Convert sklearn 'datasets bunch' object to Pandas DataFrames
 #y = pd.Series(mnist.target).astype('int').astype('category')
 #X = pd.DataFrame(mnist.data)
 
-print(X.shape)
-print(y.shape)
+print("Dataset shape: ", X.shape)
+print('Result set shape: ',y.shape)
 
 allLabels = np.unique(y)
-print(allLabels)
+print("Labels present in Dataset: ", allLabels)
+
+annotations = np.ndarray(shape=(0,2))
 
 # For each Class get the current ChallengeIDs -> generateFakeLabels
 for label in allLabels:
     challengeIDs = get_challenge_ids(label,y)
-    annotations = generate_new_labels(challengeIDs,[0,1,2],[0.2,0.2,.6], 100)
+    newann = generate_new_labels(challengeIDs,[0,1,2],[0.2,0.2,.6], 1000)
 
-    #Test
-    print("For original label: ", label)
-    print("0 count: ",np.sum(annotations[:,1] == 0))
-    print("1 count: ", np.sum(annotations[:,1] == 1))
-    print("2 count: ",np.sum(annotations[:,1] == 2))
+    annotations = np.append(annotations, newann, axis=0)
+
+#Test
+print('Shape of Annnotations', annotations.shape)
+print("0 count: ",np.sum(annotations[:,1] == 0))
+print("1 count: ", np.sum(annotations[:,1] == 1))
+print("2 count: ",np.sum(annotations[:,1] == 2))
+
 
