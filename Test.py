a61fb51e462bdcf1b12ff387717d7565c9398aae
Philipp Lüthi <philippluca.luethi@gmail.com>
2018-11-07 09:35:57 +0100

    Majority Vote finder.

--- Test.py
+++ Test.py
@@ -4,17 +4,29 @@ import numpy as np
 import os
 import random
 
+from statistic_analysis import rate_annotations_by_datapoint, transform_majority_label, certain_uncertain_split
 
 df = pd.DataFrame(data={'datapoint_id': ['a','a','a','b','a','b','b'],
                         'annotation':   ['1','1','7','5','1','5','5']})
 
-def f(group):
-    calculation = group.value_counts(normalize=True)
-    return pd.DataFrame(calculation).T
-
-dk = df.groupby('datapoint_id')['annotation'].value_counts(normalize=True).unstack().fillna(0)
+df = rate_annotations_by_datapoint(df)
+print('--------------Input-----------------')
 print(df)
-print(dk)
+
+
+certain, uncertain = certain_uncertain_split(df,0.75)
+print('--------------Certain Split-----------------')
+print(certain)
+print('--------------Uncertain Split-----------------')
+print(uncertain)
+
+
+majority_labels = transform_majority_label(certain)
+print('--------------Majority Annotation-----------------')
+print(majority_labels)
+print('-----------Majority Annotation (as np.array)-----------')
+print(majority_labels.reset_index().values)
+
 
 # https://www.google.ch/search?q=groupby+apply+getting+multiindex&spell=1&sa=X&ved=0ahUKEwjqmYSuw8DeAhXE0aQKHYtnBZAQBQgqKAA&biw=1299&bih=1040
 # https://groups.google.com/forum/#!topic/pydata/WRBsKOHganE
--- statistic_analysis.py
+++ statistic_analysis.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 
 
-def find_majority_vote() -> np.ndarray:
-    pass
-
-
-def rate_annotations_by_datapoint(annotations: np.ndarray):
+def rate_annotations_by_datapoint(annotations: np.ndarray) -> pd.DataFrame:
     df = pd.DataFrame(annotations, columns=["datapoint_id", "annotation"])
     grouped = df.groupby('datapoint_id')['annotation'].value_counts(normalize=True).unstack().fillna(0)
     return grouped
 
 
+def certain_uncertain_split(rated_annotations: pd.DataFrame, threshold: float) -> (pd.DataFrame, pd.DataFrame):
+    certain_labels = rated_annotations.max(axis=1) >= threshold
+    return rated_annotations[certain_labels], rated_annotations[certain_labels==False]
+
+
+def transform_majority_label(rated_annotations: pd.DataFrame) -> pd.DataFrame:
+    return rated_annotations.idxmax(axis=1)
