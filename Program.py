d97d019ba78c71d3b6d3d555f91f3af54dace1e7
Thibault Gagnaux <thibault.gagnaux@students.fhnw.ch>
2018-11-20 16:34:40 +0100

    Adds experiments save function

--- Program.py
+++ Program.py
@@ -1,24 +1,19 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-from create_distributed_labels import generate_new_labels_confusionmatrix
+from create_distributed_labels import generate_new_annotations_confusionmatrix
+from experiment import Experiment
 from statistic_analysis import rate_annotations_by_datapoint
 
 from sklearn.datasets import load_iris, load_digits
+
 X, y = load_iris(True)
 X, y = load_digits(return_X_y=True)
-
-# Convert sklearn 'datasets bunch' object to Pandas DataFrames
-#y = pd.Series(mnist.target).astype('int').astype('category')
-#X = pd.DataFrame(mnist.data)
 
 print("Dataset shape: ", X.shape)
 print('Result set shape: ', y.shape)
 
 allLabels = np.unique(y)
-y = y
 print("Labels present in Dataset: ", allLabels)
 
 cm = np.array([
@@ -33,14 +28,17 @@ cm = np.array([
     [0,   0,   0,   0,   0,   0,   0,   0,   1,   0],   
     [0,   0,   0,   0,   0,   0,   0,   0,   0,   1]
 ])
-annotations = generate_new_labels_confusionmatrix(cm, allLabels, y, count=1000, normalize=False)
+annotations = generate_new_annotations_confusionmatrix(cm, allLabels, y, count=1000, normalize=False)
+experiment = Experiment()
+experiment.add(cm)
+experiment.add(annotations)
+experiment.save()
 
-#Test
+
 print("--------------Annotation counts per Label--------------------")
 print('Shape of Annnotations', annotations.shape)
 for label in allLabels:
-    print(label ," count: ",np.sum(annotations[:,1] == label))
-
+    print(label, " count: ", np.sum(annotations[:, 1] == label))
 
 print("-----------Annotations array (e.g. Database export)----------")
 print(annotations)
@@ -48,6 +46,3 @@ print(annotations)
 print("------------Label certainty per datapoint-----------------")
 rated_annotations = rate_annotations_by_datapoint(annotations)
 print(rated_annotations.head(20))
-
-
-
--- create_distributed_labels.py
+++ create_distributed_labels.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.utils import resample
 
 
-def get_challenge_ids(label, list):
+def get_datapoint_ids(label, list):
     return np.array(np.where(list == label)).ravel()
 
 
@@ -11,15 +11,14 @@ def generate_new_labels(challenge_ids, labels, p, count):
     new_labels = np.random.choice(labels, count, True, p)
     return np.array([upsampled, new_labels]).transpose()
 
-
-def generate_new_labels_confusionmatrix(cm, classes, datapoints, count=1000, normalize=False):
+def generate_new_annotations_confusionmatrix(cm, classes, datapoints, count=1000, normalize=False):
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
 
     annotations = np.ndarray(shape=(0, 2))
-    for i in np.arange(0, len(classes)):
-        challengeIDs = get_challenge_ids(classes[i], datapoints)
+    for i in range(len(classes)):
+        challengeIDs = get_datapoint_ids(classes[i], datapoints)
         newann = generate_new_labels(challengeIDs, classes, cm[i], count)
         annotations = np.append(annotations, newann, axis=0)
 
