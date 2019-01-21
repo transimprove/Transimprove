0b8a3f735bc0ebfc7f64300e4942df739eb96a26
Marcel Wuersch <marcel.wuersch@unifr.ch>
2019-01-21 16:06:25 +0100

    changed config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-EXPERIMENTS_PATH = "/home/bonseyes/IP5_DataQuality/experiments/"
+EXPERIMENTS_PATH = "/home/bonseyes/IP5_DataQuality/experiments_cifar/"
 DEEP_DIVA_PATH = "/deepdiva/"
 MNIST_PATH_ORIGINAL = "/dd_resources/data/MNIST/"
+CIFAR_PATH_ORIGINAL = "/home/bonseyes/datasets/CIFAR10/"
