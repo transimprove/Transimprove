f288f829423c21f84327f4a5f6c2dc84b90c3f15
Marcel Wuersch <marcel.wuersch@fhnw.ch>
2019-02-06 14:54:15 +0100

    revamped CIFAR experiments to test for more things

--- Experiments/DeepDivaCifarExperiment.py
+++ Experiments/DeepDivaCifarExperiment.py
@@ -51,9 +51,12 @@ class DeepDivaCifarExperiment(object):
             [0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0.8]  # 9
         ])
         self.adaptor = DeepDIVADatasetAdapter(config.CIFAR_PATH_ORIGINAL)
-        self.dir_existing_model = os.path.join(self.this_resource.get_experiment_directory(), "existing_model")
-        self.dir_ground_truth_model = os.path.join(self.this_resource.get_experiment_directory(), "ground_truth_model")
-        self.dir_maj_vote_model = os.path.join(self.this_resource.get_experiment_directory(), "majority_vote_model")
+        self.dir_existing_model = os.path.join(
+            self.this_resource.get_experiment_directory(), "existing_model")
+        self.dir_ground_truth_model = os.path.join(
+            self.this_resource.get_experiment_directory(), "ground_truth_model")
+        self.dir_maj_vote_model = os.path.join(
+            self.this_resource.get_experiment_directory(), "majority_vote_model")
 
     def main(self):
         annotations_per_label = 50
@@ -69,24 +72,28 @@ class DeepDivaCifarExperiment(object):
         allLabels = "0 1 2 3 4 5 6 7 8 9".split(" ")
 
         annotations = generate_new_annotations_confusionmatrix(self.cm, allLabels, y_labels,
-                                                               count=len(y_labels) * annotations_per_label,
+                                                               count=len(
+                                                                   y_labels) * annotations_per_label,
                                                                normalize=False)
         print('Shape of Annnotations', annotations.shape)
         for label in allLabels:
             print(label, " count: ", np.sum(annotations[:, 1] == label))
 
         print("\n\n\n\n\n==============Train existing model====================")
-        existing_score, existing_model = self.train_CIFAR_DeepDIVA_Model(X_y_existing_model, self.dir_existing_model)
+        existing_score, existing_model = self.train_CIFAR_DeepDIVA_Model(
+            X_y_existing_model, self.dir_existing_model)
         y = existing_model.predict(np.atleast_2d(X_y_existing_model[:, 0]).T)
         print(y)
 
         print("\n\n\n\n\n==============Train truth model====================")
-        possible_score, _ = self.train_CIFAR_DeepDIVA_Model(X_y_remaining, self.dir_ground_truth_model)
+        possible_score, _ = self.train_CIFAR_DeepDIVA_Model(
+            X_y_remaining, self.dir_ground_truth_model)
 
         print("\n\n\n\n\n==============Pipeline Implementation====================")
         print(X_datapoints.shape)
         # Adding the ID to columns
-        datapoints_for_pipeline = np.vstack((np.arange(0, len(X_datapoints)), X_datapoints)).T
+        datapoints_for_pipeline = np.vstack(
+            (np.arange(0, len(X_datapoints)), X_datapoints)).T
 
         transimprove_pipeline = Pipeline(datapoints_for_pipeline, annotations,
                                          models=[('DeepDivaCifar', existing_model)])
@@ -94,25 +101,33 @@ class DeepDivaCifarExperiment(object):
         print("\n\n\n\n\n==============Train Majority voting Model====================")
         maj_score, _ = self.train_CIFAR_DeepDIVA_Model(transimprove_pipeline.certain_data_set(threshold=0),
                                                        self.dir_maj_vote_model)
-        maj_best_model = self.find('model_best.pth.tar', self.dir_maj_vote_model)
+        maj_best_model = self.find(
+            'model_best.pth.tar', self.dir_maj_vote_model)
+        plugin_best_model = self.find(
+            'model_best.pth.tar', self.dir_existing_model)
         print("Best Model: ", maj_best_model)
         print("Score of existing model: ", existing_score)
         print("Score of truth model: ", possible_score)
         print("Score of MajVot model: ", maj_score)
-
-        sys.exit()
         # run one experiment every 5%
-        consistencies = np.arange(0.50, 0.98, 0.1)
+        consistencies = np.arange(0.50, 0.99, 0.1)
 
         # runs multiple experiments for each consistency threshold in the defined range above
         scores_certain = []
         scores_full = []
+        scores_retrain_maj = []
+        scores_retrain_plugin = []
         std_certain = []
         std_full = []
+        std_retrain_maj = []
+        std_retrain_plugin = []
         for consistency in consistencies:
             transimprove_pipeline.fit(consistency)
             tmp_certain_scores = []
             tmp_full_scores = []
+            tmp_maj_scores = []
+            tmp_plugin_scores = []
+
             for iteration in range(1, 1):
                 # TRAIN CONSISTENT MODEL
                 score_certain, _ = self.train_CIFAR_DeepDIVA_Model(transimprove_pipeline.certain_data_set(),
@@ -124,34 +139,66 @@ class DeepDivaCifarExperiment(object):
                                                                 os.path.join(
                                                                     self.this_resource.get_experiment_directory(),
                                                                     str(consistency), 'full_ds'))
+                # RETRAIN MAJORITY VOTING MODEL
+                score_maj_retrain, _ = self.train_CIFAR_DeepDIVA_Model(X_y_data=transimprove_pipeline.certain_data_set(),
+                                                                       directory=os.path.join(self.this_resource.get_experiment_directory(),
+                                                                                              str(consistency), 'certain_ds'),
+                                                                       retrain=True,
+                                                                       retrainModel=maj_best_model)
+                # RETRAIN PLUG-IN MODEL
+                score_plugin_retrain, _ = self.train_CIFAR_DeepDIVA_Model(X_y_data=transimprove_pipeline.certain_data_set(),
+                                                                          directory=os.path.join(self.this_resource.get_experiment_directory(),
+                                                                                                 str(consistency), 'certain_ds'),
+                                                                          retrain=True,
+                                                                          retrainModel=plugin_best_model)
                 tmp_certain_scores.append(score_certain)
                 tmp_full_scores.append(score_full)
+                tmp_maj_scores.append(score_maj_retrain)
+                tmp_plugin_scores.append(score_plugin_retrain)
+
             scores_certain.append(np.average(tmp_certain_scores))
             std_certain.append(np.std(tmp_certain_scores))
             scores_full.append(np.average(tmp_full_scores))
             std_full.append(np.std(tmp_full_scores))
+            scores_retrain_maj.append(np.average(tmp_maj_scores))
+            std_retrain_maj.append(np.std(tmp_maj_scores))
+            scores_retrain_plugin.append(np.average(tmp_plugin_scores))
+            std_retrain_plugin.append(np.std(tmp_plugin_scores))
 
         self.this_resource.add(scores_certain)
         self.this_resource.add(std_certain)
         self.this_resource.add(scores_full)
         self.this_resource.add(std_full)
+        self.this_resource.add(scores_retrain_maj)
+        self.this_resource.add(std_retrain_maj)
+        self.this_resource.add(scores_retrain_plugin)
+        self.this_resource.add(std_retrain_plugin)
+        
         self.this_resource.save()
         plot_score_comparisons(self.this_resource.get_experiment_directory(), consistencies, scores_certain,
                                scores_full, std_certain, std_full,
                                possible_score, existing_score)
 
-    def train_CIFAR_DeepDIVA_Model(self, X_y_data, directory):
+    def train_CIFAR_DeepDIVA_Model(self, X_y_data, directory, retrain=False, retrainModel=None):
         deep_diva_cifar_model = DeepDIVAModelAdapter(directory, self.adaptor)
-        X_y_train, X_y_val = train_test_split(X_y_data, test_size=0.2, random_state=42)
+        X_y_train, X_y_val = train_test_split(
+            X_y_data, test_size=0.2, random_state=42)
         if np.unique(X_y_train[:, 1]).size == 10 and np.unique(X_y_val[:, 1]).size == 10:
-            self.adaptor.create_symlink_dataset(X_y_train, directory, subfolder='train')
-            self.adaptor.create_symlink_dataset(X_y_val, directory, subfolder='val')
+            self.adaptor.create_symlink_dataset(
+                X_y_train, directory, subfolder='train')
+            self.adaptor.create_symlink_dataset(
+                X_y_val, directory, subfolder='val')
             self.adaptor.copy_symlink(directory, subfolder='test')
-            train, val, test = deep_diva_cifar_model.train()
+            if not retrain:
+                train, val, test = deep_diva_cifar_model.train()
+            else:
+                trainm, val, test = deep_diva_cifar_model.retrain(
+                    model=retrainModel)
             rmtree(os.path.join(directory, 'val'))
             rmtree(os.path.join(directory, 'test'))
             for classdir in deep_diva_cifar_model.classes:
-                class_dir_full_path = os.path.join(directory, 'train', classdir)
+                class_dir_full_path = os.path.join(
+                    directory, 'train', classdir)
                 for file_name in os.listdir(class_dir_full_path):
                     file_path = os.path.join(class_dir_full_path, file_name)
                     if os.path.islink(file_path):
--- Experiments/helper/plots.py
+++ Experiments/helper/plots.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 
-def plot_score_comparisons(experiment_path, consistencies, scores_certain, scores_full, std_certain, std_full, max_possible_score, existing_score,
-                           crop_y=False):
+def plot_score_comparisons(experiment_path, consistencies, scores_certain, scores_full, std_certain, std_full, scores_retrain_maj, std_retrain_maj,
+                           scores_retrain_plugin, std_retrain_plugin, max_possible_score, existing_score, crop_y=False):
     """
     Creates the comparison plot that is used in the paper and saves it in the experiment folder. 
     Plots the different accuracy scores on the y-axis and the consistencies on the x-axis.
@@ -16,6 +16,10 @@ def plot_score_comparisons(experiment_path, consistencies, scores_certain, score
     :param scores_full: []
     :param std_certain: []
     :param std_full: []
+    :param scores_retrain_maj: []
+    :param std_retrain_mdj: []
+    :param scores_retrain_plugin: []
+    :param std_retrain_plugin: []
     :param max_possible_score: int
     :param existing_score: int
     :param crop_y: bool: if true, the cropped version which gives more detail is plotted
@@ -24,15 +28,30 @@ def plot_score_comparisons(experiment_path, consistencies, scores_certain, score
     existing_scores = np.repeat(existing_score, len(consistencies))
     fig, ax = plt.subplots()
     title = "accuracy"
-    l1 = ax.plot(consistencies, max_possible_scores, color='black', label='Upper Bound')
-    l2 = ax.plot(consistencies, existing_scores, color='green', label='Plug-in-model')
-    l3 = ax.plot(consistencies, scores_certain, color='blue', label='Consistent dataset-model')
-    ax.fill_between(consistencies, np.array(scores_certain)-np.array(std_certain),np.array(scores_certain)+np.array(std_certain), alpha=0.2, facecolor='royalblue' )
-    l4 = ax.plot(consistencies, scores_full, color='lightgreen', label='Relabeled dataset-model')
-    ax.fill_between(consistencies, np.array(scores_full)-np.array(std_full),np.array(scores_full)+np.array(std_full), alpha=0.2, facecolor='peachpuff')
+    l1 = ax.plot(consistencies, max_possible_scores,
+                 color='black', label='Upper Bound')
+    l2 = ax.plot(consistencies, existing_scores,
+                 color='green', label='Plug-in-model')
+    l3 = ax.plot(consistencies, scores_certain, color='blue',
+                 label='Consistent dataset-model')
+    ax.fill_between(consistencies, np.array(scores_certain)-np.array(std_certain),
+                    np.array(scores_certain)+np.array(std_certain), alpha=0.2, facecolor='royalblue')
+    l4 = ax.plot(consistencies, scores_full, color='lightgreen',
+                 label='Relabeled dataset-model')
+    ax.fill_between(consistencies, np.array(scores_full)-np.array(std_full),
+                    np.array(scores_full)+np.array(std_full), alpha=0.2, facecolor='peachpuff')
+    l5 = ax.plot(consistencies, scores_retrain_maj, color='light_gray',
+                 label='Retrained Majority Voting')
+    ax.fill_between(consistencies, np.array(scores_retrain_maj)-np.array(std_retrain_maj),
+                    np.array(scores_retrain_maj)+np.array(std_retrain_maj), alpha=0.2, facecolor='gainsboro')
+    l6 = ax.plot(consistencies, scores_retrain_plugin, color='red',
+                 label='Retrained Plugin-Model')
+    ax.fill_between(consistencies, np.array(scores_retrain_plugin)-np.array(std_retrain_plugin),
+                    np.array(scores_retrain_plugin)+np.array(std_retrain_plugin), alpha=0.2, facecolor='salmon')
     ax.legend(loc='lower left')
     ax.set(xlabel='Consistency (%)', ylabel='Accuracy (%)')
     ax.set_title(title, fontsize=14, fontweight='bold')
     # uncomment to show the plot instead of saving it
     # plt.show()
-    fig.savefig(os.path.join(experiment_path, title + ".png"), transparent=False, dpi=200, inches='tight', format='png')
+    fig.savefig(os.path.join(experiment_path, title + ".png"),
+                transparent=False, dpi=200, inches='tight', format='png')
